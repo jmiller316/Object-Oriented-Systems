02736565cade93fdcf43d7cccfd338d7f0e4016e
rg9507 <31706574+rg9507@users.noreply.github.com>
2017-09-17 10:09:29 -0500

    Small fixes

--- CS4773Assignment1/src/misc/RecordProcessor.java
+++ CS4773Assignment1/src/misc/RecordProcessor.java
@@ -1,199 +1,220 @@
 package misc;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class RecordProcessor {
-	private static String [] firstName;
-	private static String [] lastName;
-	private static int [] age;
-	private static String [] paymentType;
-	private static double [] pay;
+	private static int sumOfAllAges = 0;
+	private static int employeesWithCommissions = 0;
+	private static int employeesWithSalary = 0;
+	private static int employeesWithHourlyWage = 0;
+	
+	private static double sumOfAllCommissions = 0.0;
+	private static double sumOfAllSalary = 0.0;
+	private static double sumOfAllHourlyWage = 0.0;
+	private static double averageCommission = 0.0;
+	private static double averageSalary = 0.0;
+	private static double averageHourlyWage = 0.0;
+	private static double averageAge = 0.0;
 
-	public static String processFile(String inputFile) {
-		StringBuffer ouput = new StringBuffer();
-
-		Scanner readFile = null;
+	private static ArrayList<Employee> allEmployees = new ArrayList<Employee>();	
+	private static final int NUMBER_OF_COLUMNS = 5;
+	
+	public static void main(String [] args) {	
+		String inputFile = "";
 		try {
-			readFile = new Scanner(new File(inputFile));
-		} catch (FileNotFoundException e) {
-			System.err.println(e.getMessage());
-			return null;
+			inputFile = args[0];
+		} catch (Exception e) {
+			System.err.println("Argument Exception");
 		}
-
+		System.out.println(processFile(inputFile));		
+	}	
+	
+	/**
+	 * Takes the file and checks to see if the file is empty if not it stores the information and 
+	 * formated function to correctly format
+	 * @param inputFile
+	 * @return
+	 */
+	public static String processFile(String inputFile) {	
+		StringBuffer output = new StringBuffer();
+		ArrayList<String> inputFileLines = new ArrayList<String>();  
 		int numberOfLinesInFile = 0;
-		while(readFile.hasNextLine()) {
-			String readLine = readFile.nextLine();
-			if(readLine.length() > 0)
-				numberOfLinesInFile++;
-		}
-
-		firstName = new String[numberOfLinesInFile];
-		lastName = new String[numberOfLinesInFile];
-		age = new int[numberOfLinesInFile];
-		paymentType = new String[numberOfLinesInFile];
-		pay = new double[numberOfLinesInFile];
-
-		readFile.close();
-		try {
-			readFile = new Scanner(new File(inputFile));
-		} catch (FileNotFoundException e) {
-			System.err.println(e.getMessage());
-			return null;
-		}
-
-		numberOfLinesInFile = 0;
-		while(readFile.hasNextLine()) {
-			String readLine = readFile.nextLine();
-			if(readLine.length() > 0) {
-
-				String [] wordsInLine = readLine.split(",");
-
-				int wordLocationInLine = 0;
-				for(;wordLocationInLine < lastName.length; wordLocationInLine++) {
-					if(lastName[wordLocationInLine] == null)
-						break;
-
-					if(lastName[wordLocationInLine].compareTo(wordsInLine[1]) > 0) {
-						for(int i = numberOfLinesInFile; i > wordLocationInLine; i--) {
-							firstName[i] = firstName[i - 1];
-							lastName[i] = lastName[i - 1];
-							age[i] = age[i - 1];
-							paymentType[i] = paymentType[i - 1];
-							pay[i] = pay[i - 1];
-						}
-						break;
-					}
-				}
-
-				firstName[wordLocationInLine] = wordsInLine[0];
-				lastName[wordLocationInLine] = wordsInLine[1];
-				paymentType[wordLocationInLine] = wordsInLine[3];
-
-				try {
-					age[wordLocationInLine] = Integer.parseInt(wordsInLine[2]);
-					pay[wordLocationInLine] = Double.parseDouble(wordsInLine[4]);
-				} catch(Exception e) {
-					System.err.println(e.getMessage());
-					readFile.close();
-					return null;
-				}
-
-				numberOfLinesInFile++;
-			}
-		}
-
+		getAllLinesInFile(inputFile, inputFileLines);	
+		numberOfLinesInFile = inputFileLines.size();
+		
 		if(numberOfLinesInFile == 0) {
-			System.err.println("No records found in data file");
-			readFile.close();
 			return null;
-		}
-
-		//print the rows
-		ouput.append(String.format("# of people imported: %d\n", firstName.length));
-
-		ouput.append(String.format("\n%-30s %s  %-12s %12s\n", "Person Name", "Age", "Emp. Type", "Pay"));
-		for(int i = 0; i < 30; i++)
-			ouput.append(String.format("-"));
-		ouput.append(String.format(" ---  "));
-		for(int i = 0; i < 12; i++)
-			ouput.append(String.format("-"));
-		ouput.append(String.format(" "));
-		for(int i = 0; i < 12; i++)
-			ouput.append(String.format("-"));
-		ouput.append(String.format("\n"));
-
-		for(int i = 0; i < firstName.length; i++) {
-			ouput.append(String.format("%-30s %-3d  %-12s $%12.2f\n", firstName[i] + " " + lastName[i], age[i]
-				, paymentType[i], pay[i]));
-		}
-
-		int sumOfAllAges = 0;
-		float avegareAge = 0f;
-		int wordLocationInLine = 0;
-		double sumOfAllCommissions = 0;
-		double averageComission = 0;
-		int employeesWithHourlyWage = 0;
-		double sumOfAllHourlyWage = 0;
-		double averageHourlyWage = 0;
-		int employeesWithSalary = 0;
-		double sumOfAllSalary = 0;
-		double averageSalary = 0;
-		for(int i = 0; i < firstName.length; i++) {
-			sumOfAllAges += age[i];
-			if(paymentType[i].equals("Commission")) {
-				sumOfAllCommissions += pay[i];
-				wordLocationInLine++;
-			} else if(paymentType[i].equals("Hourly")) {
-				sumOfAllHourlyWage += pay[i];
-				employeesWithHourlyWage++;
-			} else if(paymentType[i].equals("Salary")) {
-				sumOfAllSalary += pay[i];
-				employeesWithSalary++;
-			}
-		}
-		avegareAge = (float) sumOfAllAges / firstName.length;
-		ouput.append(String.format("\nAverage age:         %12.1f\n", avegareAge));
-		averageComission = sumOfAllCommissions / wordLocationInLine;
-		ouput.append(String.format("Average commission:  $%12.2f\n", averageComission));
-		averageHourlyWage = sumOfAllHourlyWage / employeesWithHourlyWage;
-		ouput.append(String.format("Average hourly wage: $%12.2f\n", averageHourlyWage));
-		averageSalary = sumOfAllSalary / employeesWithSalary;
-		ouput.append(String.format("Average salary:      $%12.2f\n", averageSalary));
-
-		HashMap<String, Integer> hashMapOfMultipleFirstNames = new HashMap<String, Integer>();
-		int sameFirstName = 0;
-		for(int i = 0; i < firstName.length; i++) {
-			if(hashMapOfMultipleFirstNames.containsKey(firstName[i])) {
-				hashMapOfMultipleFirstNames.put(firstName[i], hashMapOfMultipleFirstNames.get(firstName[i]) + 1);
-				sameFirstName++;
-			} else {
-				hashMapOfMultipleFirstNames.put(firstName[i], 1);
-			}
-		}
-
-		ouput.append(String.format("\nFirst names with more than one person sharing it:\n"));
-		if(sameFirstName > 0) {
-			Set<String> set = hashMapOfMultipleFirstNames.keySet();
-			for(String multipleFirstName : set) {
-				if(hashMapOfMultipleFirstNames.get(multipleFirstName) > 1) {
-					ouput.append(String.format("%s, # people with this name: %d\n", multipleFirstName, hashMapOfMultipleFirstNames.get(multipleFirstName)));
-				}
-			}
-		} else {
-			ouput.append(String.format("All first names are unique"));
-		}
-
-		HashMap<String, Integer> hashMapOfMultipleLastNames = new HashMap<String, Integer>();
-		int sameLastName = 0;
-		for(int i = 0; i < lastName.length; i++) {
-			if(hashMapOfMultipleLastNames.containsKey(lastName[i])) {
-				hashMapOfMultipleLastNames.put(lastName[i], hashMapOfMultipleLastNames.get(lastName[i]) + 1);
-				sameLastName++;
-			} else {
-				hashMapOfMultipleLastNames.put(lastName[i], 1);
-			}
-		}
-
-		ouput.append(String.format("\nLast names with more than one person sharing it:\n"));
-		if(sameLastName > 0) {
-			Set<String> set = hashMapOfMultipleLastNames.keySet();
-			for(String multipleLastName : set) {
-				if(hashMapOfMultipleLastNames.get(multipleLastName) > 1) {
-					ouput.append(String.format("%s, # people with this name: %d\n", multipleLastName, hashMapOfMultipleLastNames.get(multipleLastName)));
-				}
-			}
-		} else {
-			ouput.append(String.format("All last names are unique"));
-		}
-
-		//close the file
-		readFile.close();
-
-		return ouput.toString();
+		}	
+		if(!getDataFromFile(numberOfLinesInFile, inputFileLines))
+			return null;
+		formatEmployeeData(numberOfLinesInFile, output);	
+		return output.toString();	
+	}
+	
+	/**
+	 * stores each line of the file
+	 * @param inputFile
+	 * @param inputFileLines
+	 */
+	public static void getAllLinesInFile(String inputFile, ArrayList<String> inputFileLines) {
+		BufferedReader readFile = null;
+		String currentLine;	
+		try {
+			readFile = new BufferedReader(new FileReader(inputFile));
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}	
+		try { 
+			while ((currentLine = readFile.readLine()) != null) {
+				if(!currentLine.isEmpty())
+					inputFileLines.add(currentLine);
+			} 
+		} catch (IOException e) {
+				e.printStackTrace();
+		}		
 	}
 
+	/**
+	 * gets the data from each line
+	 * @param numberOfLinesInFile
+	 * @param inputFileLines
+	 */
+	private static boolean getDataFromFile(int numberOfLinesInFile, ArrayList<String> inputFileLines) {		
+		String currentLine = null;
+		String [] wordsInLine = null;
+		
+		for(int i = 0; i < numberOfLinesInFile; i++) {
+			currentLine = inputFileLines.get(i);
+			wordsInLine = currentLine.split(",");
+			if(wordsInLine.length != NUMBER_OF_COLUMNS) {
+				throw (new RuntimeException("Wrong number of columns"));
+			}
+				
+			if(!setIndividualEmployeeData(wordsInLine, i))
+				return false;
+		}	
+		return true;
+	}
+	
+	/**
+	 * Used to set all the information of each file to the information type
+	 * @param wordsInLine
+	 * @param lineNumber
+	 */
+	private static boolean setIndividualEmployeeData(String [] wordsInLine, int lineNumber) {	
+		Employee employee = new Employee();
+		employee.setFirstName(wordsInLine[0]);
+		employee.setLastName(wordsInLine[1]);
+		employee.setPaymentType(wordsInLine[3]);
+		try {
+			employee.setAge(Integer.parseInt(wordsInLine[2]));
+			employee.setPay(Double.parseDouble(wordsInLine[4]));
+		} catch(Exception e) {
+			return false;
+		}
+		allEmployees.add(employee);
+		addToEmployeeCalculations(employee);
+		return true;
+	}
+	
+	/**
+	 * Adds the correct employee payment type to each employee
+	 * @param employee
+	 */
+	private static void addToEmployeeCalculations(Employee employee) {	
+		sumOfAllAges += employee.getAge();
+		switch(employee.getPaymentType()) {
+			case "Commission":
+				sumOfAllCommissions += employee.getPay();
+				employeesWithCommissions++;
+				break;
+			case "Salary":
+				sumOfAllSalary += employee.getPay();
+				employeesWithSalary++;
+				break;
+			case "Hourly":
+				sumOfAllHourlyWage += employee.getPay();
+				employeesWithHourlyWage++;
+				break;
+			default: 
+				throw (new RuntimeException("Error, not a valid payment type"));
+		}
+	}
+	
+	/**
+	 * Sorts employees by proper format
+	 * @param numberOfLinesInFile
+	 * @param output
+	 */
+	public static void formatEmployeeData(int numberOfLinesInFile, StringBuffer output) {
+		List<Employee> sortedEmployees = new ArrayList<Employee>(allEmployees);
+		Collections.sort(sortedEmployees, new Employee.LastNameComparator());
+		output.append(String.format("# of people imported: %d\n", numberOfLinesInFile));
+		output.append(String.format("\n%-30s %s  %-12s %12s\n", "Person Name", "Age", "Emp. Type", "Pay" ));
+		output.append(String.format("------------------------------ ---  ------------ ------------\n"));
+	
+		for(int i = 0; i < numberOfLinesInFile; i++) {
+			Employee currentEmployee = sortedEmployees.get(i);
+			output.append(String.format("%-30s %-3d  %-12s $%12.2f\n", currentEmployee.getFirstName() + " " + currentEmployee.getLastName()
+				,currentEmployee.getAge(), currentEmployee.getPaymentType(), currentEmployee.getPay()));
+		}	
+		calculateAverages(numberOfLinesInFile, output);
+		compareNames(numberOfLinesInFile, output);
+	}
+	
+	/**
+	 * Calculates the average of all employees information
+	 * @param numberOfLinesInFile
+	 * @param output
+	 */
+	public static void calculateAverages(int numberOfLinesInFile, StringBuffer output) {	
+		averageAge = (float) sumOfAllAges / numberOfLinesInFile;
+		averageCommission = sumOfAllCommissions / employeesWithCommissions;
+		averageHourlyWage = sumOfAllHourlyWage / employeesWithHourlyWage;
+		averageSalary = sumOfAllSalary / employeesWithSalary;
+		output.append(String.format("\nAverage age:         %12.1f\n", averageAge));
+		output.append(String.format("Average commission:  $%12.2f\n", averageCommission));
+		output.append(String.format("Average hourly wage: $%12.2f\n", averageHourlyWage));
+		output.append(String.format("Average salary:      $%12.2f\n" , averageSalary));	
+	}
+	
+	/**
+	 *Sorts names and counts the number of times each name is used
+	 * @param numberOfLinesInFile
+	 * @param output
+	 */
+	public static void compareNames(int numberOfLinesInFile, StringBuffer output) {
+		List<String> firstNames = new ArrayList<String>();
+		List<String> lastNames = new ArrayList<String>();
+		int numberOfSameFirstNames = 0;
+		int numberOfSameLastNames = 0;
+		for(int i = 0; i < numberOfLinesInFile; i++) {
+			firstNames.add(allEmployees.get(i).getFirstName());
+			lastNames.add(allEmployees.get(i).getLastName());
+		}
+		Set<String> uniqueFirstNames = new HashSet<String>(firstNames);
+		Set<String> uniqueLastNames = new HashSet<String>(lastNames);
+		output.append(String.format("\nFirst names with more than one person sharing it:\n"));
+		
+		for(String key: uniqueFirstNames) {
+			numberOfSameFirstNames = Collections.frequency(firstNames, key);
+			if(numberOfSameFirstNames > 1)
+				output.append(String.format("%s, # people with this name: %d\n", key, numberOfSameFirstNames));
+		}
+		output.append(String.format("\nLast names with more than one person sharing it:"));
+		
+		for(String key: uniqueLastNames) {
+			numberOfSameLastNames = Collections.frequency(lastNames, key);
+			if(numberOfSameLastNames > 1)
+				output.append(String.format("\n%s, # people with this name: %d\n", key, numberOfSameLastNames));
+		}
+	}
 }
